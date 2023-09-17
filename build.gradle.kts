667250b06a0c265a9d985ce00ef249724728d2c5
matsuurayuki1219 <yuki.matsuura@progrit.co.jp>
2023-09-18 00:22:39 +0900

    fix: sdk import error.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,6 @@ plugins {
     id("com.google.dagger.hilt.android") version "2.47" apply false
     id("com.google.devtools.ksp") version "1.8.10-1.0.9" apply false
     id("com.google.gms.google-services") version "4.3.15" apply false
+    id("com.google.firebase.crashlytics") version "2.9.9" apply false
+    id("com.google.firebase.firebase-perf") version "1.4.2" apply false
 }
